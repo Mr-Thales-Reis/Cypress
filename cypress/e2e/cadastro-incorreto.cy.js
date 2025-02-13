5b88e0602f66fe2577a76426ec64748b5107345a
Thales Reis <thalesreis.es@gmail.com>
2025-02-13 10:53:24 -0300

    Nessa branch eu executei comandos que testam as mensagens do formulário, me certificando que elas estivessem funcionando. Por esse motivo o nome do arquivo é caminho incorreto, ele não faz o preenchimento dos campos pois o esperado é que o sistema não permita o avanço e me mostre as mensagens de erro

--- cypress/e2e/cadastro-incorreto.cy.js
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -1,7 +1,10 @@
 describe('Página de Cadastro', () => {
-    it('', () => {
-      cy.visit('https://adopet-frontend-cypress.vercel.app/cadastro')
-      
-      cy.contains('button', 'Cadastrar').click();
-    })
+  it('Preencher os campos do formulário incorretamente e exibir mensagem ao usuário', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/cadastro')
+    cy.get('[data-test="input-name"]').type('Thales dos Reis');
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains('É necessário informar um endereço de email').should('be.visible');
+    cy.contains('Crie uma senha').should('be.visible');
+    cy.contains('Repita a senha criada acima').should('be.visible');
   })
+})
