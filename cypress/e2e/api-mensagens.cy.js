0e9b46988668ae852cdd140dd5a36bbb1770a0b1
Thales Reis <thalesreis.es@gmail.com>
2025-02-18 18:37:16 -0300

    Update api-mensagens.cy.js

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,15 +1,15 @@
-describe('Api Adopet', ()=>{
-    const authorization = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjNTZlODBmYS1jMzFmLTQyOGEtODU3Mi1lODQzYzUwYWI5ODciLCJhZG9wdGVyTmFtZSI6Im9sYSIsImlhdCI6MTczOTkwMDIzNCwiZXhwIjoxNzQwMTU5NDM0fQ.rtmoGdsDrSqVuFxYmX0dwUb_nk6CgfDSRwP2kJ_EY6M`
-
-    it('Mensagem de testes da API', ()=>{
+describe('Api Adopet', () => {
+     
+     it('Mensagens da API', () => {
         cy.request({
-            method: 'GET',
-            url: 'https://adopet-frontend-cypress.vercel.app/static/css/main.https://adopet-frontend-cypress.vercel.app/static/media/message.a71fd916fbc1491a5831.svg.css',
-            Headers: {authorization},
-        }).then((res)=>{
-            expect(res.status).to.be.equal(200)
-            expect(res.body).is.not.empty
-            expect(res.body).to.have.propery('msg')
-        })
+             method: 'GET' ,
+             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+             headers: cypress.env()
+                   
+                }).then((res) => {
+                    expect(res.status).to.be.equal(200)
+                    expect(res.body).is.not.empty
+                    expect(res.body).to.have.property('msg')
+                })
     })
 })
